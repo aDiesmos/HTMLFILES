c8c71dfa4b6820da23993e24af6c5a71856598f2
Jay <jalen36111@gmail.com>
2026-02-24 19:14:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6432,21 +6432,21 @@ const games = [
     isNew: true
 },
         {
-    id: 915,
+    id: 916,
     title: "Bad Ice Cream",
     image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/whatver/bad-ice-cream.png",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/whatver/bad-ice-cream.html",
     isNew: true
 },
         {
-    id: 915,
+    id: 917,
     title: "Bad Ice Cream 2",
     image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/whatver/bad-ice-cream-2.png",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/whatver/bad-ice-cream-2.html",
     isNew: true
 },
         {
-    id: 915,
+    id: 918,
     title: "Bad Ice Cream 3",
     image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/whatver/bad-ice-cream-3.png",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/whatver/bad-ice-cream-3.html",
