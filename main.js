ccd0751b06626aaeb4afca1f610fe6d472d94889
Jay <jalen36111@gmail.com>
2026-03-02 16:26:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6441,63 +6441,63 @@ const games = [
          {
     id: 917,
     title: "Wheely",
-    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/images@main/wheel.jpg",
+    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheel.jpg",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheely.html",
     isNew: true
              },
        {
     id: 918,
     title: "Wheely 2",
-    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/images@main/wheel2.jpg",
+    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheel2.jpg",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheely2.html",
     isNew: true
              },
        {
     id: 919,
     title: "Wheely 3",
-    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/images@main/wheel3.jpg",
+    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheel3.jpg",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheely3.html",
     isNew: true
              },
        {
     id: 920,
     title: "Wheely 4",
-    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/images@main/wheel4.jpg",
+    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheel4.jpg",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheely4.html",
     isNew: true
              },
        {
     id: 921,
     title: "Wheely 5",
-    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/images@main/wheel5.jpg",
+    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheel5.jpg",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheely5.html",
     isNew: true
              },
        {
     id: 922,
     title: "Wheely 6",
-    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/images@main/wheel6.jpg",
+    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheel6.jpg",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheely6.html",
     isNew: true
              },
        {
     id: 923,
     title: "Wheely 7",
-    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/images@main/wheel7.jpg",
+    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheel7.jpg",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheely7.html",
     isNew: true
              },
        {
     id: 924,
     title: "Wheely 8",
-    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/images@main/wheel8.jpg",
+    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheel8.jpg",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/wheely8.html",
     isNew: true
              },
          {
     id: 925,
     title: "Sonic Revert",
-    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/images@main/revert.jpg",
+    image: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/revert.jpg",
     url: "https://cdn.jsdelivr.net/gh/waycrosspublicmedia/HTMLFILES@main/2026uploads/sonicrevert.html",
     isNew: true
 },
